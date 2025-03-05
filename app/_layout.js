814aa2ea6532f3ae4f7a452a8a2837fc9807c339
J0020git <ralph.jerome.hernandez@outlook.com>
2025-03-05 18:20:18 +1100

    init

---
+++ app/_layout.js
@@ -0,0 +1,18 @@
+import { Tabs } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+    </Tabs>
+  );
+}
