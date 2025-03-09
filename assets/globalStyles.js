68ce495e68bc5aeebc20116a4f8d977307e0ff32
J0020git <ralph.jerome.hernandez@outlook.com>
2025-03-09 21:39:00 +1100

    feat: Text styling

---
+++ assets/globalStyles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export default styles = StyleSheet.create({
+  text: {
+    fontSize: 12,
+    fontFamily: "Roboto",
+    fontWeight: 400,
+  },
+  textNav: {
+    fontSize: 12,
+    fontFamily: "Roboto",
+    fontWeight: 500,
+  },
+  textSubheading: {
+    fontSize: 16,
+    fontFamily: "Roboto",
+    fontWeight: 500,
+  },
+  textHeading: {
+    fontSize: 24,
+    fontFamily: "Roboto",
+    fontWeight: 700,
+  },
+});
